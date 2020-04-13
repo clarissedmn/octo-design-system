6916e7b5855a050955282afd4e3eff7986af0b9d
clarissedmn <clarisse.damon@gmail.com>
2020-04-13 23:26:17 +0200

    fixup! fixup! Adding Button component and rollup scss configuration

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,8 +1,4 @@
 import MyComponent from "./components/MyComponent";
-import Signature from "./components/Illustration";
-import Acronym from "./components/Acronym";
-import CodedLogo from "./components/Coded";
-import CorporateLogo from "./components/Corporate";
 import Button from "./components/Button";
 import "./colors.scss";
-export { MyComponent, Signature, Acronym, CodedLogo, CorporateLogo, Button };
+export { MyComponent, Button };
